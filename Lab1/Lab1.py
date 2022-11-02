27a5b3098db4c7c0fbfa07f3ae0da672433646d6
Hugo Spégagne <hspegagne@gmail.com>
2022-11-02 15:04:18 +0100

    Salt and pepper

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -25,6 +25,16 @@ def gaussianNoise(image,sigma):
             image[i,j] = res 
     return image
 
+def saltAndPepperNoise(image,rang):
+    for i in range(rang):
+        color = 0
+        if(randint(0, 1)): 
+            color = 255
+        i = randint(0, image.shape[0]-1)
+        j = randint(0, image.shape[1]-1)
+        image[i,j] = color
+    return image
+
 def showHisto(image):
     plt.figure()
     plt.title("Histogram")
@@ -38,18 +48,23 @@ def showImageGray(image,string):
 def performance(image1, image2):
     return 0
  
-im = data.coins()   
+#im = data.coins()   
 #im = io.imread('Images/Brain.tif')
+im = io.imread('Images/Chest_CT.tif')
 
 showImageGray(im,'Brain')
 
 im_gauss = gaussianNoise(im, 50)
+im_salt = saltAndPepperNoise(im, 100)
+
 showImageGray(im_gauss, 'Gaussian noise')
+showImageGray(im_salt, 'Salt and pepper noise')
 
 ##############################################################################
 
-im_nlm_filter = res.denoise_nl_means(im_gauss)
-showImageGray(im_nlm_filter,'NLM filter')
+#im_nlm_filter = res.denoise_nl_means(im_gauss)
+#im_nlm_filter = res.denoise_nl_means(im_salt_and_pepper)
+#showImageGray(im_nlm_filter,'NLM filter')
 
 ##############################################################################
 
