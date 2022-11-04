0c9494770b18799566dca92742ec409a4ee0aff9
Hugo Spégagne <hspegagne@gmail.com>
2022-11-04 21:00:06 +0100

    Update Lab1.py

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -6,7 +6,7 @@ Created on Wed Oct 26 11:37:23 2022
 """
 
 from skimage import io 
-from skimage import restoration as res
+from skimage.restoration import denoise_nl_means
 from skimage import data
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,23 +17,25 @@ from medpy.filter.smoothing import anisotropic_diffusion
 plt.close('all')
 
 def gaussianNoise(image,sigma):
+    im = image.copy()
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             res = -1
             while res < 0 or res > 256 :
                 res = gauss(image[i,j], sigma)
-            image[i,j] = res 
-    return image
+            im[i,j] = res 
+    return im
 
 def saltAndPepperNoise(image,rang):
+    im = image.copy()
     for i in range(rang):
         color = 0
         if(randint(0, 1)): 
             color = 255
         i = randint(0, image.shape[0]-1)
         j = randint(0, image.shape[1]-1)
-        image[i,j] = color
-    return image
+        im[i,j] = color
+    return im
 
 def showHisto(image):
     plt.figure()
@@ -45,29 +47,106 @@ def showImageGray(image,string):
     plt.title(string)
     plt.imshow(image, cmap='gray')
     
-def performance(image1, image2):
+def performance(image1, image2): ####!!!!!!!###########
     return 0
- 
-#im = data.coins()   
-#im = io.imread('Images/Brain.tif')
-im = io.imread('Images/Chest_CT.tif')
 
-showImageGray(im,'Brain')
-
-im_gauss = gaussianNoise(im, 50)
-im_salt = saltAndPepperNoise(im, 100)
-
-showImageGray(im_gauss, 'Gaussian noise')
-showImageGray(im_salt, 'Salt and pepper noise')
-
+def Question1(image):
+    fig, axes = plt.subplots(2, 4, figsize=(8, 8), sharex=False, sharey=True)
+    ax = axes.ravel()
+    
+    ax[0].set_title("Gauss, sigma = 10")
+    ax[0].imshow(gaussianNoise(image, 10), cmap='gray')
+    
+    ax[1].set_title("Gauss, sigma = 20")
+    ax[1].imshow(gaussianNoise(image, 20), cmap='gray')
+    
+    ax[2].set_title("Gauss, sigma = 40")
+    ax[2].imshow(gaussianNoise(image, 40), cmap='gray')
+    
+    ax[3].set_title("Gauss, sigma = 80")
+    ax[3].imshow(gaussianNoise(image, 80), cmap='gray')
+    
+    ax[4].set_title("Salt and pepper, range = 100")
+    ax[4].imshow(saltAndPepperNoise(image, 100), cmap='gray')
+    
+    ax[5].set_title("Salt and pepper, range = 1000")
+    ax[5].imshow(saltAndPepperNoise(image, 1000), cmap='gray')
+    
+    ax[6].set_title("Salt and pepper, range = 10000")
+    ax[6].imshow(saltAndPepperNoise(image, 10000), cmap='gray')
+    
+    ax[7].set_title("Salt and pepper, range = 100000")
+    ax[7].imshow(saltAndPepperNoise(image, 100000), cmap='gray')
+    
+    for a in ax:
+        a.set_axis_off()
+    
+    plt.show()
+    
+def Question2(image):
+    fig, axes = plt.subplots(3, 4, figsize=(8, 8), sharex=False, sharey=True)
+    ax = axes.ravel()
+    
+    im1 = gaussianNoise(image, 10)
+    im2 = gaussianNoise(image, 20)
+    im3 = gaussianNoise(image, 40)
+    im4 = gaussianNoise(image, 80)
+    
+    im5 = saltAndPepperNoise(image, 100)
+    im6 = saltAndPepperNoise(image, 1000)
+    im7 = saltAndPepperNoise(image, 10000)
+    im8 = saltAndPepperNoise(image, 100000)
+    
+    ax[0].set_title("NLM filtering gauss")
+    ax[0].imshow(denoise_nl_means(im1), cmap='gray')
+    
+    ax[1].imshow(denoise_nl_means(im2), cmap='gray')
+    
+    ax[2].imshow(denoise_nl_means(im3), cmap='gray')
+    
+    ax[3].imshow(denoise_nl_means(im4), cmap='gray')
+    
+    ax[4].set_title("NLM filtering S&P")
+    ax[4].imshow(denoise_nl_means(im5), cmap='gray')
+    
+    ax[5].imshow(denoise_nl_means(im6), cmap='gray')
+    
+    ax[6].imshow(denoise_nl_means(im7), cmap='gray')
+    
+    ax[7].imshow(denoise_nl_means(im8), cmap='gray')
+    
+    ax[8].set_title("NLM filtering S&P")
+    ax[8].imshow(denoise_nl_means(im5), cmap='gray')
+    
+    ax[9].imshow(denoise_nl_means(im6), cmap='gray')
+    
+    ax[10].imshow(denoise_nl_means(im7), cmap='gray')
+    
+    ax[11].imshow(denoise_nl_means(im8), cmap='gray')
+    
+    for a in ax:
+        a.set_axis_off()
+    
+    plt.show()
+    
 ##############################################################################
-
-#im_nlm_filter = res.denoise_nl_means(im_gauss)
-#im_nlm_filter = res.denoise_nl_means(im_salt_and_pepper)
-#showImageGray(im_nlm_filter,'NLM filter')
+ 
+im1 = io.imread('Images/Brain.tif')
+im2 = io.imread('Images/Chest_CT.tif')
 
 ##############################################################################
 
 # im_aniso = anisotropic_diffusion(im)
 # showImageGray(im_aniso,'Anisotropic filter')
+
+################Question 1####################################################
+
+Question1(im1)
+
+################Question 2####################################################
+
+#Question2(im1)
+
+################Question 3####################################################
+
 
