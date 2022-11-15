066c749c83c8c0124a68d913ed13d84bdddca795
Hugo Spégagne <hspegagne@gmail.com>
2022-11-15 11:23:15 +0100

    Update Lab2

--- Lab2/Snake.py
+++ Lab2/Snake.py
@@ -12,26 +12,112 @@ from skimage import data
 from skimage.filters import gaussian
 from skimage.segmentation import active_contour
 from skimage import io
+import matplotlib
+from skimage import data, img_as_float
+from skimage import exposure
+
 
 plt.close('all')
 
 # img = data.astronaut()
 # img = rgb2gray(img)
-img=io.imread('Images/Brain.tif')
+
+
+#########################################""
+
+
+
+matplotlib.rcParams['font.size'] = 8
+
+
+def plot_img_and_hist(image, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+
+    """
+    image = img_as_float(image)
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(image, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+
+    # Display histogram
+    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel('Pixel intensity')
+    ax_hist.set_xlim(0, 1)
+    ax_hist.set_yticks([])
+
+    # Display cumulative distribution
+    img_cdf, bins = exposure.cumulative_distribution(image, bins)
+    ax_cdf.plot(bins, img_cdf, 'r')
+    ax_cdf.set_yticks([])
+
+    return ax_img, ax_hist, ax_cdf
+
+
+# Load an example image
+img = io.imread('Images/toto.tif')
+img = np.array(img, dtype='int')
+
+# Contrast stretching
+p2, p98 = np.percentile(img, (2, 98))
+img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
+
+# Equalization
+img_eq = exposure.equalize_hist(img)
+
+# Adaptive Equalization
+img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
+
+# Display results
+fig = plt.figure(figsize=(8, 5))
+axes = np.zeros((2, 4), dtype=np.object)
+axes[0, 0] = fig.add_subplot(2, 4, 1)
+for i in range(1, 4):
+    axes[0, i] = fig.add_subplot(2, 4, 1+i, sharex=axes[0,0], sharey=axes[0,0])
+for i in range(0, 4):
+    axes[1, i] = fig.add_subplot(2, 4, 5+i)
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
+ax_img.set_title('Low contrast image')
+
+y_min, y_max = ax_hist.get_ylim()
+ax_hist.set_ylabel('Number of pixels')
+ax_hist.set_yticks(np.linspace(0, y_max, 5))
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_rescale, axes[:, 1])
+ax_img.set_title('Contrast stretching')
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_eq, axes[:, 2])
+ax_img.set_title('Histogram equalization')
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_adapteq, axes[:, 3])
+ax_img.set_title('Adaptive equalization')
+
+ax_cdf.set_ylabel('Fraction of total intensity')
+ax_cdf.set_yticks(np.linspace(0, 1, 5))
+
+# prevent overlap of y-axis labels
+fig.tight_layout()
+plt.show()
+
+################################################
 
 s = np.linspace(0, 2*np.pi, 400)
 r = 145 + 45*np.sin(s)
 c = 175 + 45*np.cos(s)
 init = np.array([r, c]).T
 
-snake = active_contour(gaussian(img, 3, preserve_range=False),
-                       init, alpha=0.0001, beta=10, gamma=0.001)
+snake = active_contour(gaussian(img_eq, 3, preserve_range=False),
+                        init, alpha=0.0001, beta=10, gamma=0.001)
 
-fig, ax = plt.subplots(figsize=(7, 7))
-ax.imshow(img, cmap=plt.cm.gray)
+fig, ax = plt.subplots(figsize=(15, 15))
+ax.imshow(img_eq, cmap=plt.cm.gray)
 ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
 ax.plot(snake[:, 1], snake[:, 0], '-b', lw=3)
 ax.set_xticks([]), ax.set_yticks([])
-ax.axis([0, img.shape[1], img.shape[0], 0])
+ax.axis([0, img_eq.shape[1], img_eq.shape[0], 0])
 
 plt.show()
